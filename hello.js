1f6e984a1d106ef513e3641242bafd6748f696b4
Anuradha Chowdhury <anuradhachowdhury955@gmail.com>
2022-04-17 22:50:24 +0600

    all is well

---
+++ hello.js
@@ -0,0 +1,59 @@
+// console.log(21);
+// var name="Anu Radha"
+// console.log(name);
+// var bananprice=12;
+// console.log(bananprice);
+// var isHot=true;
+// var isRich=false;
+// console.log(isHot);
+// var myname="Hero Alam";
+// var myname="Hero ";
+// var myname=" Alam";
+// console.log(myname);
+// var promise=" I promise I will work hard";
+// console.log(promise.toLowerCase());
+// console.log(promise.indexOf('work'));
+// console.log(promise.split('I'));
+// console.log(promise.split(' '));
+// var num1=29;
+// var num2=30.5;
+// var total=num1+num2;
+// total=total.toFixed(3)
+// console.log(total);
+// var name1='Justin';
+// var name2='Bieber';
+// console.log(name1+name2);
+
+// var number=-60;
+// var result1=Math.abs(number);
+// console.log(result1);
+// var number=6.4510;
+// var result1=Math.round(number);
+// console.log(result1);
+
+// var number1=5.214;
+// var result2=Math.random(number1);
+// console.log(result2);
+// var price=7;
+// if(price<10){
+//     console.log("I will eat");
+// }
+// else{
+//     console.log("I will not eat");
+// }
+// var jobPaiso=true;
+// var salary=50000;
+// if(jobPaiso==true && salary>25000){
+//     console.log('OK');
+// }
+// else{
+//     console.log('Sorry');
+// }
+// var friendsAge=[12,13,15,23,16,12];
+// console.log(friendsAge[3]);
+var friendsAge=[12,13,15,23,16,12];
+//  friendsAge.push(17);
+//  friendsAge.push(40);
+
+friendsAge.pop();
+console.log(friendsAge);
